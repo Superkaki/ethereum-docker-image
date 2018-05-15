5a193c640431b010459c78a7aaaaca704750d168
Inaki <inaki.seco@opendeusto.es>
2018-05-15 13:04:53 +0200

    building modified

--- build.sh
+++ build.sh
@@ -2,28 +2,25 @@
 
 if [ "$2" = "restart" ]
 then
-### Removing previous ejecutions ###
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker image rm ethereum-network-node
-#
-#### Creating image ###
-docker build -t ethereum-network-node ./
+    ### Removing previous ejecutions ###
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker image rm ethereum-network-node
+    
+    #### Creating image ###
+    docker build -t ethereum-network-node ./
 fi
 
 ### Running nodes ###
 counter=1
 while [ $counter -le $1 ]
 do
-docker stop "node$counter"
-docker rm "node$counter"
-echo Running node "node$counter"
-docker run -d --name "node$counter"  ethereum-network-node --rpcaddr 0.0.0.0
-gnome-terminal --tab -e "docker exec -it 'node$counter' sh"
-((counter++))
+    docker stop "node$counter"
+    docker rm "node$counter"
+    echo Running node "node$counter"
+    docker run -d --name "node$counter"  ethereum-network-node --rpcaddr 0.0.0.0
+    gnome-terminal --tab -e "docker exec -it 'node$counter' sh"
+    ((counter++))
 done
 
 echo DONE!
-#
-#
-#docker exec -it "node$1" sh
